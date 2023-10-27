6c5f0dc9749df462a973ffd0b34fee0e38e7fb10
olaszakos <olaszakos@gmail.com>
2023-10-27 11:37:20 +0200

    mobile nav

--- frontend/src/frontend/src/lib/components/Navbar.svelte
+++ frontend/src/frontend/src/lib/components/Navbar.svelte
@@ -16,6 +16,8 @@
   import LogoutIcon from "./icons/LogoutIcon.svelte";
   import { page } from "$app/stores";
   import UploadIcon from "./icons/UploadIcon.svelte";
+  import { fade, fly } from "svelte/transition";
+  import CloseIcon from "./icons/CloseIcon.svelte";
 
   let isOpen = false;
   let isOpenRegistrationModal = false;
@@ -24,6 +26,7 @@
   let authClientValue;
   let firstNameValue;
   let isAuthenticatedValue;
+  let showMobileMenu = true;
 
   actor.subscribe((value) => (actorValue = value));
   authClient.subscribe(async (value) => {
@@ -86,10 +89,11 @@
 </script>
 
 {#if isAuthenticatedValue !== null}
-  <nav class="bg-background-200 rounded-b-3xl">
-    <div class="flex h-16 items-center max-w-5xl mx-auto px-4">
-      <a href="/">
-        <img src="/logo.svg" alt="" />
+  <nav class="bg-background-200 rounded-b-3xl relative z-20">
+    <div class="flex h-14 md:h-16 items-center max-w-5xl mx-auto px-4">
+      <a href="/" class="shrink-0">
+        <img src="/logo.svg" alt="" class="hidden md:block" />
+        <img src="/mobile-logo.svg" alt="" class="md:hidden" />
       </a>
       {#if firstNameValue}
         <div class="flex ml-2">
@@ -110,7 +114,28 @@
           Login
         </button>
       {:else if $isAuthenticated === true}
-        <div class="flex gap-8">
+        <button
+          class="flex flex-col items-stretch gap-[5px] md:hidden w-5 h-5"
+          on:click={() => (showMobileMenu = !showMobileMenu)}
+        >
+          <span
+            class="h-[2px] bg-accent-100 rounded-full transition-transform {showMobileMenu
+              ? 'rotate-45 translate-y-[7px]'
+              : 'rotate-0'}"
+          />
+          <span
+            class="h-[2px] bg-accent-100 rounded-full transition-opacity {showMobileMenu
+              ? 'opacity-0'
+              : 'opacity-100'}"
+          />
+          <span
+            class="h-[2px] bg-accent-100 rounded-full transition-transform {showMobileMenu
+              ? '-rotate-45 translate-y-[-7px]'
+              : 'rotate-0'}"
+          />
+        </button>
+
+        <div class="hidden md:flex gap-8">
           <a
             href="/"
             class="btn btn-ghost"
@@ -143,6 +168,58 @@
       {/if}
     </div>
   </nav>
+
+  {#if showMobileMenu}
+    <div
+      class="fixed inset-0 bg-black/50"
+      transition:fade={{ duration: 200 }}
+    />
+    <div
+      transition:fly={{ duration: 300, x: 1000, opacity: 1 }}
+      class="fixed md:hidden  inset-0 bg-background-300 z-10 pt-16"
+    >
+      <div class="p-4 flex flex-col gap-4 h-full">
+        <a
+          href="/"
+          class="btn btn-ghost justify-start"
+          class:btn-ghost-active={$page.route.id === "/"}
+          on:click={() => (showMobileMenu = false)}
+        >
+          <IconFile />
+          Files</a
+        >
+        <a
+          href="/upload"
+          class="btn btn-ghost justify-start"
+          class:btn-ghost-active={$page.route.id === "/upload"}
+          on:click={() => (showMobileMenu = false)}
+        >
+          <UploadIcon />
+          Upload</a
+        >
+        <a
+          href="/requests"
+          class="btn btn-ghost justify-start"
+          class:btn-ghost-active={$page.route.id === "/requests"}
+          on:click={() => (showMobileMenu = false)}
+        >
+          <RequestsIcon />
+          Requests</a
+        >
+        <div class="flex-1" />
+        <button
+          on:click={() => {
+            handleLogout();
+            showMobileMenu = false;
+          }}
+          class="btn btn-ghost  justify-start"
+        >
+          <LogoutIcon />
+          Logout</button
+        >
+      </div>
+    </div>
+  {/if}
 {/if}
 
 <RegistrationModal isOpen={isOpenRegistrationModal} />
